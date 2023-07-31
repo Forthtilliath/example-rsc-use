4b43e2e85e96e0296bb2199d61183cd290766d58
Forthtilliath <forth.developer@gmail.com>
2023-07-31 23:06:57 +0200

    video finished

---
+++ src/_components/FoodProvider.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { PropsWithChildren, createContext, useContext } from "react";
+
+export type FoodContextType = Promise<string[]>;
+type ProviderProps = PropsWithChildren<{
+  foodPromise: FoodContextType;
+}>
+
+const FoodContext = createContext<FoodContextType | null>(null);
+
+export const useFoodPromise = () => {
+  const food = useContext(FoodContext);
+  if (!food) {
+    throw new Error("useFood must be used within a FoodProvider");
+  }
+
+  return food;
+};
+
+const FoodProvider = ({
+  children,
+  foodPromise,
+}: ProviderProps) => {
+  return (
+    <FoodContext.Provider value={foodPromise}>{children}</FoodContext.Provider>
+  );
+};
+
+export default FoodProvider;
